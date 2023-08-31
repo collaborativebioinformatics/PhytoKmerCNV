10e2d6711f8d63c8e45e19975398c5222da0f2bf
Christian Brueffer <christian@brueffer.io>
2023-08-31 23:30:27 +0200

    Add resources to set up an execution environment for the project
    Adds a setup script that installs a conda distribution and a conda
    environment for the project.

---
+++ scripts/setup_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#
+# Installs the Mambaforge conda distribution and adds the specified project environment.
+#
+
+CONDA_PREFIX=~/miniconda3
+ENV_FILE=../environment.yml
+
+cd /tmp/
+curl -LO https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
+bash Mambaforge-Linux-x86_64.sh -b -u -p $CONDA_PREFIX
+cd
+
+$CONDA_PREFIX/bin/mamba env create --file $ENV_FILE
